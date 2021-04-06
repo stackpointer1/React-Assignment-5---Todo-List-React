b96e119b0d7c6d0083e4e1b919295a83b9bfddde
jyoti <jostring9199@gmail.com>
2021-04-07 02:03:40 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,72 @@
 import React from "react";
-import "./../styles/App.css";
+import { useState } from "react";
 
-function App() 
-{
-	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
-	</div>
-	);
+import TodoList from "./TodoList";
+function App() {
+  const [item, setItem] = useState("");
+  const [itemArr, setItemArr] = useState([]);
+
+  const itemValue = (event) => {
+    setItem(event.target.value);
+  };
+
+  const changeValue = () => {
+    if (item.trim() !== "") {
+      setItemArr([...itemArr, item]);
+      setItem("");
+    }
+  };
+  const deleteitem = (id) => {
+    setItemArr((preValue) => {
+      return preValue.filter((itemArr, index) => {
+        return index !== id;
+      });
+    });
+  };
+  const updateItem = (id, editValue) => {
+    // console.log(id);
+    // console.log(editValue);
+    if (editValue !== "") {
+      setItemArr((preValu) => {
+        preValu[id] = editValue;
+        return [...preValu];
+      });
+    }
+  };
+
+  return (
+    <div id="main">
+      <div className="todoList">
+        <br />
+        <input
+          type="text"
+          placeholder="enter the text"
+          id="task"
+          value={item}
+          onChange={itemValue}
+        />
+        <button id="btn" onClick={changeValue}>
+          {" "}
+          +{" "}
+        </button>
+        <ol>
+          {itemArr.map((itemCur, index) => {
+            return (
+              <TodoList
+                className="list"
+                classNameDelete="delete"
+                classNameEdit="Edit"
+                key={index}
+                id={index}
+                text={itemCur}
+                onSelect={deleteitem}
+                onEdit={updateItem}
+              />
+            );
+          })}
+        </ol>
+      </div>
+    </div>
+  );
 }
-
-
 export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { useState } from "react";
+
+
+const ToDoLists = (props) => {
+  const [editInput, setEditInput] = useState("");
+  const [addInEdit, setAddInEdit] = useState({
+    show: false,
+  });
+
+  const EditItem = () => {
+    console.log("edit");
+    setAddInEdit({ show: true });
+  };
+  const funEditUpdate = (event) => {
+    setEditInput(event.target.value.trim());
+  };
+  return (
+    <>
+      <div className="todo_style">
+        <li className={props.className}>{props.text}</li>
+        <button
+          className={props.classNameDelete}
+          onClick={() => {
+            props.onSelect(props.id);
+          }}
+        >
+          delete
+        </button>
+        {addInEdit.show ? (
+          <>
+            <br />
+            <input
+              type="text"
+              className="editTask"
+              placeholder="Edit"
+              onChange={funEditUpdate}
+            />
+            <button
+              className="saveTask"
+              onClick={() => {
+                props.onEdit(props.id, editInput);
+                setAddInEdit({ show: false });
+              }}
+            >
+              +
+            </button>
+          </>
+        ) : (
+          <button className={props.classNameEdit} onClick={EditItem}>
+            edit
+          </button>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default ToDoLists;
